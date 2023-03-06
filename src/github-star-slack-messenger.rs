6bfb4a0f29366fe4e9a79bf31e717ccfa5a2004b
Ji Chen <jaykchen@gmail.com>
2023-03-06 09:33:49 -0500

    init

---
+++ src/github-star-slack-messenger.rs
@@ -0,0 +1,29 @@
+use github_flows::{listen_to_event, EventPayload};
+use slack_flows::send_message_to_channel;
+
+#[no_mangle]
+#[tokio::main(flavor = "current_thread")]
+pub async fn run() -> anyhow::Result<()> {
+    let owner: &str = "jaykchen";
+    let repo: &str = "a-test";
+
+    listen_to_event(owner, repo, vec!["star"], |payload| handler(repo, payload)).await;
+
+    Ok(())
+}
+
+async fn handler(repo: &str, payload: EventPayload) {
+    let slack_workspace_name: &str = "ik8";
+    let slack_channel_name: &str = "general";
+
+    if let EventPayload::UnknownEvent(e) = payload {
+        let stargazers_count = e["repository"]["stargazers_count"].as_i64().unwrap_or(-1);
+
+        let text =
+            format!("Congratulations on your repository {repo} with {stargazers_count} stars.");
+
+        if stargazers_count % 10 == 0 {
+            send_message_to_channel(slack_workspace_name, slack_channel_name, text);
+        }
+    }
+}
